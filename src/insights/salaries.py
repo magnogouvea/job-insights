71c5d6262146dc2234df27d8d115423d1e9b5e54
Magno Gouvêa <94480802+magnogouvea@users.noreply.github.com>
2023-02-27 16:41:21 -0300

    Feat: Implementa a função get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,16 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    salaries = read(path)
+    max_salary = 0
+    for salary in salaries:
+        if salary["max_salary"].isdigit() and int(salary["max_salary"]) > int(
+            max_salary
+        ):
+            max_salary = salary["max_salary"]
+    return int(max_salary)
 
 
 def get_min_salary(path: str) -> int:
