6d259c974cc61a317d755090f9ab097a505fe63c
Magno Gouvêa <94480802+magnogouvea@users.noreply.github.com>
2023-02-27 16:38:23 -0300

    Feat: Implementa a função get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    industries = read(path)
+    unique_industries = []
+    for ind in industries:
+        if ind["industry"] not in unique_industries and ind["industry"] != "":
+            unique_industries.append(ind["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
