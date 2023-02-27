b46bd169c5002e20841bf5f669c3663477167321
Magno Gouvêa <94480802+magnogouvea@users.noreply.github.com>
2023-02-27 16:36:53 -0300

    Feat: Implementa a função filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -21,18 +21,7 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    filtered_list_of_jobs = [
+        job for job in jobs if job["job_type"] == job_type
+    ]
+    return filtered_list_of_jobs
