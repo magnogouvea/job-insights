df4072e54a145001628c19d55d716947b1151b66
Magno Gouvêa <94480802+magnogouvea@users.noreply.github.com>
2023-02-27 16:39:52 -0300

    Feat: Implementa a função filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -12,18 +12,7 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    filtered_list_of_industry = [
+        ind for ind in jobs if ind["industry"] == industry
+    ]
+    return filtered_list_of_industry
